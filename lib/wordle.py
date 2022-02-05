dca1e9a8c046770a1dce203407f7b4853751cf7b
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2022-02-05 14:44:36 +0300

    Update wordle.py

--- lib/wordle.py
+++ lib/wordle.py
@@ -64,6 +64,7 @@ def update_data(num, successful, imported=True):
 
     if num != -1:
         jsondata["correct in"][num - 1] += 1
+        jsondata["total guesses"] += (num - 1)
 
     else:
         jsondata["total guesses"] += 6
